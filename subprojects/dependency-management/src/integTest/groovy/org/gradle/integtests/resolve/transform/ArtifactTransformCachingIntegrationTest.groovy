6fbd0139565274b664951a586d61b9fad938c357
Marc Philipp <marc@gradle.com>
2018-06-05 09:36:17 +0200

    Use constants for cache directory names for robust tests

--- subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/transform/ArtifactTransformCachingIntegrationTest.groovy
+++ subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/transform/ArtifactTransformCachingIntegrationTest.groovy
@@ -1462,7 +1462,7 @@ allprojects {
 
     List<TestFile> outputDirs(String from, String to) {
         List<TestFile> dirs = []
-        def baseDir = cacheDir.file("files-1.1", from).absolutePath + File.separator
+        def baseDir = cacheDir.file(CacheLayout.TRANSFORMS_STORE.getKey(), from).absolutePath + File.separator
         def pattern = Pattern.compile("Transforming " + Pattern.quote(from) + " to " + Pattern.quote(to) + " into (" + Pattern.quote(baseDir) + "\\w+)")
         for (def line : output.readLines()) {
             def matcher = pattern.matcher(line)
